8f2a1ff1234ce987925722d21d728574fd8ae99d
robharper <robert.harper@gmail.com>
2023-11-12 08:18:49 -0500

    Updating regenerate image to be able to just generate without upload

--- _job/regenerate.py
+++ _job/regenerate.py
@@ -5,17 +5,18 @@ from job.gcp import upload_image
 
 GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 BUCKET_NAME = os.getenv("IMAGES_BUCKET_NAME")
+IMAGES_PATH = os.getenv("IMAGES_PATH")
 
-def regenerate_image(prompt, date, dry_run=False):
+def regenerate_image(prompt, date, dry_run=False, fetch_only=False):
     # Create the inspirational image using DALL-E
     image = generate_image(prompt, dry_run=dry_run)
 
     # image = build_image(quote_data, image)
 
     # Upload the image to Cloud Storage
-    image_url = upload_image(project_id=GCP_PROJECT_ID, bucket_name=BUCKET_NAME, filename=f"{date}.jpg", image=image, dry_run=dry_run)
-
-    return image_url
+    if not fetch_only:
+        image_url = upload_image(project_id=GCP_PROJECT_ID, bucket_name=BUCKET_NAME, filename=f"{IMAGES_PATH}/{date}.jpg", image=image, dry_run=dry_run)
+        return image_url
 
 
 if __name__ == "__main__":
@@ -23,8 +24,9 @@ if __name__ == "__main__":
     parser.add_argument('prompt')
     parser.add_argument('date')
     parser.add_argument('-d', '--dryrun', action='store_true')
+    parser.add_argument('-f', '--fetch', action='store_true')
 
     args = parser.parse_args()
 
-    result = regenerate_image(args.prompt, args.date, args.dryrun)
+    result = regenerate_image(args.prompt, args.date, dry_run=args.dryrun, fetch_only=args.fetch)
     print(result)
